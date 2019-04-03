68e3b28ca7f7736c38fd2b12429edeea09b716f5
YangArvin <39477122+YangArvin@users.noreply.github.com>
2019-04-03 14:51:25 +0800

    Update FirstApplication.java

--- src/main/java/com/springboot/first/FirstApplication.java
+++ src/main/java/com/springboot/first/FirstApplication.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class FirstApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FirstApplication.class, args);
 	}
